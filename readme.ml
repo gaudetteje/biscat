e60b567d964681d835226d3a965259b413dd7738
Jason Gaudette <gaudetteje@users.noreply.github.com>
2018-07-08 16:31:23 -0400

    Update readme.ml

--- readme.ml
+++ readme.ml
@@ -1,4 +1,4 @@
-#BiSCAT - The Binaural Spectrogram Correlation and Transformation Receiver Model
+# BiSCAT - The Binaural Spectrogram Correlation and Transformation Receiver Model
 
 BiSCAT is an auditory modeling framework that is intended to be modular and expandable.  Most auditory system models assume a filterbank separates the sound waveform at each ear, but the level of detail following this stage varies.  BiSCAT models the auditory response in a deterministic fashion following the original SCAT model, but it also allows for detailed simulations using probablistic neuronal implementations such as Ray Meddis' inner hair cell model and/or an integrate-and-fire network of neurons immediately following the spiking auditory nerve cells.
 
@@ -9,21 +9,21 @@ Directory Contents
 ==============================================
 
 Files:
-biscat.m  -  main GUI interface to the modeling framework
-config.mat  -  The working configuration data file loaded at startup
-readme.txt  -  This text document
-runBiscatMain.m  -  main function that integrates all submodels; This can be invoked via BiSCAT GUI or the command line
++ biscat.m  -  main GUI interface to the modeling framework
++ config.mat  -  The working configuration data file loaded at startup
++ readme.txt  -  This text document
++ runBiscatMain.m  -  main function that integrates all submodels; This can be invoked via BiSCAT GUI or the command line
 
 Subfolders:
-config_files  -  Supplementary configuration data files
-gui_callbacks  -  Supporting functions used by the BiSCAT GUI
++ config_files  -  Supplementary configuration data files
++ gui_callbacks  -  Supporting functions used by the BiSCAT GUI
 
-sig_generate  -  Functions to generate and plot input signals (Panel 1)
-coch_filters  -  Filterbank functions to model basilar membrane motion (Panel 2)
-neur_discharge  -  Acoustic-to-neural transduction models by IHCs (Panel 2)
-neur_temporal  -  Temporal processing models and delay lines (Panel 3)
-neur_spectral  -  Spectral processing models (Panel 3)
-testbed_files  -  General data files used in developing model algorithms
++ sig_generate  -  Functions to generate and plot input signals (Panel 1)
++ coch_filters  -  Filterbank functions to model basilar membrane motion (Panel 2)
++ neur_discharge  -  Acoustic-to-neural transduction models by IHCs (Panel 2)
++ neur_temporal  -  Temporal processing models and delay lines (Panel 3)
++ neur_spectral  -  Spectral processing models (Panel 3)
++ testbed_files  -  General data files used in developing model algorithms
 
 
 ==============================================
